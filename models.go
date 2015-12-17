39d3255a793198b8c25eb829f0720a74c28e3ea0
Jordan Peterson <dysolution@gmail.com>
2015-12-17 00:45:45 -0600

    group all receiver methods together for Batch, etc.

--- models.go
+++ models.go
@@ -18,50 +18,13 @@ type Contribution struct{ context *cli.Context }
 
 func (b Batch) Index()       { get(Batches) }
 func (b Batch) Get()         { get(b.path()) }
-func (b Batch) Create()      { post(buildBatch(b.context), Batches) }
-func (b Batch) Update()      { put(buildBatchUpdate(b.context), b.path()) }
+func (b Batch) Create()      { post(b.build(b.context), Batches) }
+func (b Batch) Update()      { put(b.buildUpdate(), b.path()) }
 func (b Batch) Delete()      { _delete(b.path()) }
 func (b Batch) path() string { return Batches + "/" + getBatchID(b.context) }
 func (b Batch) id() string   { return getRequiredValue(b.context, "submission-batch-id") }
 
-func (r Release) Index()     { get(childPath("releases", r.context, "")) }
-func (r Release) Get()       { get(childPath("releases", r.context, r.id())) }
-func (r Release) Create()    { post(r.build(r.context), batchPath(r.context)+"/releases") }
-func (r Release) Delete()    { _delete(childPath("releases", r.context, r.id())) }
-func (r Release) id() string { return getRequiredValue(r.context, "release-id") }
-
-func (c Contribution) Index()     { get(childPath("contributions", c.context, "")) }
-func (c Contribution) Get()       { get(childPath("contributions", c.context, c.id())) }
-func (c Contribution) Create()    { post(c.build(c.context), batchPath(c.context)+"/contributions") }
-func (c Contribution) Delete()    { _delete(childPath("contributions", c.context, c.id())) }
-func (c Contribution) id() string { return getRequiredValue(c.context, "contribution-id") }
-
-//func (c Contribution) Update() { put(buildContributionUpdate(c.context), c.path()) }
-// Token is a memoizing wrapper for the API's token-providing function.
-func Token() models.Token {
-	if token != "" {
-		return token
-	} else {
-		token = client.GetToken()
-		return token
-	}
-}
-
-func getClient(key, secret, username, password string) models.Client {
-	return models.Client{
-		models.Credentials{
-			APIKey:      key,
-			APISecret:   secret,
-			ESPUsername: username,
-			ESPPassword: password,
-		},
-		uploadBucket,
-	}
-}
-
-// buildBatch takes a Context of CLI-provided values
-// and returns a SubmissionBatch as defined by the models.
-func buildBatch(c *cli.Context) models.SubmissionBatch {
+func (b Batch) build(c *cli.Context) models.SubmissionBatch {
 	return models.SubmissionBatch{
 		SubmissionName:        c.String("submission-name"),
 		SubmissionType:        c.String("submission-type"),
@@ -73,20 +36,20 @@ func buildBatch(c *cli.Context) models.SubmissionBatch {
 	}
 }
 
-// buildBatchUpdate takes a Context of CLI-provided values
-// and returns a SubmissionBatch with the new data in the mutable fields.
-func buildBatchUpdate(c *cli.Context) models.SubmissionBatchUpdate {
+func (b Batch) buildUpdate() models.SubmissionBatchUpdate {
 	return models.SubmissionBatchUpdate{
 		models.SubmissionBatchChanges{
-			SubmissionName: c.String("submission-name"),
-			Note:           c.String("note"),
+			SubmissionName: b.context.String("submission-name"),
+			Note:           b.context.String("note"),
 		},
 	}
 }
 
-type Serializable interface {
-	Marshal() ([]byte, error)
-}
+func (r Release) Index()     { get(childPath("releases", r.context, "")) }
+func (r Release) Get()       { get(childPath("releases", r.context, r.id())) }
+func (r Release) Create()    { post(r.build(r.context), batchPath(r.context)+"/releases") }
+func (r Release) Delete()    { _delete(childPath("releases", r.context, r.id())) }
+func (r Release) id() string { return getRequiredValue(r.context, "release-id") }
 
 func (release Release) build(c *cli.Context) models.Release {
 	return models.Release{
@@ -100,6 +63,14 @@ func (release Release) build(c *cli.Context) models.Release {
 		ModelGender:          c.String("model-gender"),
 	}
 }
+
+func (c Contribution) Index()     { get(childPath("contributions", c.context, "")) }
+func (c Contribution) Get()       { get(childPath("contributions", c.context, c.id())) }
+func (c Contribution) Create()    { post(c.build(c.context), batchPath(c.context)+"/contributions") }
+func (c Contribution) Delete()    { _delete(childPath("contributions", c.context, c.id())) }
+func (c Contribution) id() string { return getRequiredValue(c.context, "contribution-id") }
+
+//func (c Contribution) Update() { put(buildContributionUpdate(c.context), c.path()) }
 
 func (contribution Contribution) build(c *cli.Context) models.Contribution {
 	return models.Contribution{
@@ -125,4 +96,30 @@ func (contribution Contribution) build(c *cli.Context) models.Contribution {
 		UploadBucket:         uploadBucket,
 		UploadId:             c.String("upload-id"),
 	}
+}
+
+// Token is a memoizing wrapper for the API's token-providing function.
+func Token() models.Token {
+	if token != "" {
+		return token
+	} else {
+		token = client.GetToken()
+		return token
+	}
+}
+
+func getClient(key, secret, username, password string) models.Client {
+	return models.Client{
+		models.Credentials{
+			APIKey:      key,
+			APISecret:   secret,
+			ESPUsername: username,
+			ESPPassword: password,
+		},
+		uploadBucket,
+	}
+}
+
+type Serializable interface {
+	Marshal() ([]byte, error)
 }

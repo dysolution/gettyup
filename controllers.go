95545759c29b6cc8109640e2a2e463338768d466
Jordan Peterson <dysolution@gmail.com>
2015-12-17 00:33:41 -0600

    move API endpoint paths to separate file

--- controllers.go
+++ controllers.go
@@ -7,19 +7,6 @@ import (
 
 var uploadBucket string
 
-const (
-	Batches            string = "/submission/v1/submission_batches"
-	ControlledValues   string = "/submission/v1/controlled_values/index"
-	Keywords           string = "/submission/v1/keywords/getty"
-	Personalities      string = "/submission/v1/personalities"
-	TranscoderMappings string = "/submission/v1/video_transcoder_mapping_values"
-)
-const (
-	Compositions   string = "/submission/v1/people_metadata/compositions"
-	Expressions    string = "/submission/v1/people_metadata/expressions"
-	NumberOfPeople string = "/submission/v1/people_metadata/number_of_people"
-)
-
 func GetKeywords(context *cli.Context) {
 	//TODO: use search input from context
 	get(Keywords)
---
+++ endpoints.go
@@ -0,0 +1,14 @@
+package main
+
+const (
+	Batches            string = "/submission/v1/submission_batches"
+	ControlledValues   string = "/submission/v1/controlled_values/index"
+	Keywords           string = "/submission/v1/keywords/getty"
+	Personalities      string = "/submission/v1/personalities"
+	TranscoderMappings string = "/submission/v1/video_transcoder_mapping_values"
+)
+const (
+	Compositions   string = "/submission/v1/people_metadata/compositions"
+	Expressions    string = "/submission/v1/people_metadata/expressions"
+	NumberOfPeople string = "/submission/v1/people_metadata/number_of_people"
+)

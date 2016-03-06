b2b2c643f7033d4030d4fd2da4bd0b1b0d95d9ff
Jordan Peterson <dysolution@gmail.com>
2016-03-06 01:39:44 -0600

    fix: remove extraneous functions

--- controlled_values.go
+++ controlled_values.go
@@ -5,20 +5,6 @@ import (
 	"github.com/dysolution/espsdk"
 )
 
-// GetKeywords requests suggestions from the Getty controlled vocabulary
-// for the keywords provided.
-//TODO: use search input from context
-func GetKeywords(context *cli.Context) *espsdk.TermList {
-	return client.GetTermList(Keywords)
-}
-
-// GetPersonalities requests suggestions from the Getty controlled vocabulary
-// for the famous personalities provided.
-//TODO: use search input from context
-func GetPersonalities(context *cli.Context) *espsdk.TermList {
-	return client.GetTermList(Personalities)
-}
-
 // GetControlledValues returns complete lists of values and descriptions for
 // fields with controlled vocabularies, grouped by submission type.
 func GetControlledValues(context *cli.Context) espsdk.ControlledValues {
@@ -47,13 +33,6 @@ func registerCVCmds() {
 		},
 	})
 	app.Commands = append(app.Commands, cli.Command{
-		Name:  "keywords",
-		Usage: "controlled vocabularies for describing Contributions",
-		Action: func(c *cli.Context) {
-			GetKeywords(c)
-		},
-	})
-	app.Commands = append(app.Commands, cli.Command{
 		Name:  "people",
 		Usage: "controlled vocabularies and values for metadata about people",
 		Subcommands: []cli.Command{
@@ -76,13 +55,6 @@ func registerCVCmds() {
 				Usage: "all known values for Number Of People",
 				Action: func(c *cli.Context) {
 					pp(client.GetTermList(NumberOfPeople))
-				},
-			},
-			{
-				Name:  "personalities",
-				Usage: "controlled vocabularies for describing famous personalities",
-				Action: func(c *cli.Context) {
-					GetPersonalities(c)
 				},
 			},
 		},
